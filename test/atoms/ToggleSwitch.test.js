ae7b069c133a2b338910287e0b320db87d331fe3
Esteban <garciawarneckee@gmail.com>
2018-12-19 19:37:02 +0100

    ToggleSwitch on test

--- test/atoms/ToggleSwitch.test.js
+++ test/atoms/ToggleSwitch.test.js
@@ -6,10 +6,18 @@ import ToggleSwitch from '../../lib/atoms/ToggleSwitch/ToggleSwitch';
 
 describe('<ToggleSwitch />', () => {
 
-  it('renders with default theme', () => {
+  it('renders off with default theme', () => {
     const wrapper = shallow(<ToggleSwitch enabled={false}/>);
     expect(wrapper.find('.switch')).to.have.lengthOf(1);
     expect(wrapper.find('.switch-toggle')).to.have.lengthOf(1);
+    expect(wrapper.find('.switch-toggle--off')).to.have.lengthOf(1);
+  });
+
+  it('renders on with default theme', () => {
+    const wrapper = shallow(<ToggleSwitch enabled={true}/>);
+    expect(wrapper.find('.switch')).to.have.lengthOf(1);
+    expect(wrapper.find('.switch-toggle')).to.have.lengthOf(1);
+    expect(wrapper.find('.switch-toggle--on')).to.have.lengthOf(1);
   });
 
 })
