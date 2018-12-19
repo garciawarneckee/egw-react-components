3cc02e5adf17be19ebfc84aee6abfccf2785a827
Esteban <garciawarneckee@gmail.com>
2018-12-19 20:21:06 +0100

    other test for rounded images

--- test/atoms/RoundedImage.test.js
+++ test/atoms/RoundedImage.test.js
@@ -14,4 +14,22 @@ describe('<RoundedImage />', () => {
     expect(wrapper.find('img').prop('src')).to.equal('test-file-stub');
   });
 
+  it('reders with explicit small size and default image', () => {
+    const wrapper = shallow(<RoundedImage size="small"/>);
+    expect(wrapper.instance().state.currentSize).to.equal('small');
+    expect(wrapper.find('img').prop('src')).to.equal('test-file-stub');
+  });
+
+  it('reders with medium size and default image', () => {
+    const wrapper = shallow(<RoundedImage size="medium"/>);
+    expect(wrapper.instance().state.currentSize).to.equal('medium');
+    expect(wrapper.find('img').prop('src')).to.equal('test-file-stub');
+  });
+
+  it('reders with large size and default image', () => {
+    const wrapper = shallow(<RoundedImage size="large"/>);
+    expect(wrapper.instance().state.currentSize).to.equal('large');
+    expect(wrapper.find('img').prop('src')).to.equal('test-file-stub');
+  });
+
 });
