f9614385ce6aa08c05913dd2dec6892b7397cc7f
Esteban <garciawarneckee@gmail.com>
2018-12-19 19:43:24 +0100

    importing isString helper and render on, off, default theme and graphite-small theme tests spplited

--- test/atoms/ToggleSwitch.test.js
+++ test/atoms/ToggleSwitch.test.js
@@ -6,18 +6,34 @@ import ToggleSwitch from '../../lib/atoms/ToggleSwitch/ToggleSwitch';
 
 describe('<ToggleSwitch />', () => {
 
-  it('renders off with default theme', () => {
-    const wrapper = shallow(<ToggleSwitch enabled={false}/>);
+  it('renders with default theme', () => {
+    const wrapper = shallow(<ToggleSwitch />);
     expect(wrapper.find('.switch')).to.have.lengthOf(1);
+    expect(wrapper.find('.switch--default')).to.have.lengthOf(1);
     expect(wrapper.find('.switch-toggle')).to.have.lengthOf(1);
+  })
+
+  it('renders with graphite-small theme', () => {
+    const wrapper = shallow(<ToggleSwitch  theme='graphite-small'/>);
+    expect(wrapper.find('.switch')).to.have.lengthOf(1);
+    expect(wrapper.find('.switch--graphite-small')).to.have.lengthOf(1);
+    expect(wrapper.find('.switch-toggle')).to.have.lengthOf(1);
+  });
+
+  it('renders off', () => {
+    const wrapper = shallow(<ToggleSwitch enabled={false}/>);
+    expect(wrapper.find('.switch-toggle')).to.have.lengthOf(1);
+    expect(wrapper.find('.switch-toggle--on')).to.have.lengthOf(0);
     expect(wrapper.find('.switch-toggle--off')).to.have.lengthOf(1);
   });
 
-  it('renders on with default theme', () => {
+  it('renders on', () => {
     const wrapper = shallow(<ToggleSwitch enabled={true}/>);
-    expect(wrapper.find('.switch')).to.have.lengthOf(1);
     expect(wrapper.find('.switch-toggle')).to.have.lengthOf(1);
     expect(wrapper.find('.switch-toggle--on')).to.have.lengthOf(1);
+    expect(wrapper.find('.switch-toggle--off')).to.have.lengthOf(0);
   });
+
+
 
 })
